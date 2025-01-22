6badd0695edf0f88371f63bdcf23de94107c161f
Ed-P41z <pai23072@uvg.edu.gt>
2025-01-22 17:56:45 -0600

    Seguí el ejemplo realizado en clase
    Se realizó el procedimiento visto en clase.

--- EjemploClase22Enero2025/EjemploClase22Enero2025/main.asm
+++ EjemploClase22Enero2025/EjemploClase22Enero2025/main.asm
@@ -1,12 +1,20 @@
-;
-; EjemploClase22Enero2025.asm
-;
-; Created: 1/22/2025 5:16:58 PM
-; Author : edvin
-;
+/*
+* EjemploClase22Enero2025.asm
+*
+* Created: 1/22/2025 5:16:58 PM
+* Author : Pedro Castillo
+*/
+.include "M328PDEF.inc"		//Include definitions specific to ATMega328P
+/***************************************************/
 
+START:
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+SETUP:
+	/******** PIN CONFIGURATION ********/
+	LDI		R16, 0xFF
+	OUT		PORTD, R16
+	OUT		PORTB, R16
+
+MAIN_LOOP:
+	DEC		R16
+	RJMP	MAIN_LOOP
