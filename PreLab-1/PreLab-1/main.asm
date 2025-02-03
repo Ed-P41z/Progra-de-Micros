3019117a115f1d3762e232c30225a7e4e926d67b
Ed-P41z <pai23072@uvg.edu.gt>
2025-02-02 19:33:47 -0600

    Configuraciones iniciales
    Se colocan algunas configuraciones iniciales para el microcontrolador, como configurar la pila.

--- PreLab-1/PreLab-1/main.asm
+++ PreLab-1/PreLab-1/main.asm
@@ -1,12 +1,20 @@
-;
-; PreLab-1.asm
-;
-; Created: 2/2/2025 7:02:46 PM
-; Author : edvin
-;
+/*
+*	PreLab-1.asm
+*
+*	Creado: 2/2/2025 7:02:46 PM
+*	Autor: Edvin Paiz
+*	Descripción: El prelab 1 consiste en un sumador con antirebotes integrado
+*/
+.include "M328PDEF.inc"
+
+.cseg
+.org 0x0000
+
+// Se configura la pila
+LDI		R16, LOW(RAMEND)
+OUT		SPL, R16
+LDI		R16, HIGH(RAMEND)
+OUT		SPH, R16
 
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+
