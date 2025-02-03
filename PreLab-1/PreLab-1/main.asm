0b6f4f6a918b5a001609898513d7cde787d48d49
Ed-P41z <pai23072@uvg.edu.gt>
2025-02-02 21:45:48 -0600

    Implementación antirrebote
    Se implementa una medida de antirrebote utilizando un delay y comparando los valores anteriores y actuales que los registros poseen sobre los botones. Si son iguales los valores, significa que fue un error de lectura; si los valores son diferentes, significa que el botón está presionado, falta implementar una medida de verificación para ver si el botón en efecto está presionado o se dejó de presionar.

--- PreLab-1/PreLab-1/main.asm
+++ PreLab-1/PreLab-1/main.asm
@@ -23,15 +23,37 @@ SETUP:
 	LDI		R16, 0x00
 	OUT		DDRD, R16	// Se configura el puerto D como entrada
 	LDI		R16, 0xFF
-	LDI		PORTD, R16 // Se configuran los pines con pull-up activado
+	OUT		PORTD, R16	// Se configuran los pines con pull-up activado
 
 	// Se configura PORTB como salida inicialmente apagado
 	LDI		R16, 0xFF
-	OUT		DDRB, R16 // Se configura el puerto B como salida
+	OUT		DDRB, R16	// Se configura el puerto B como salida
 	LDI		R16, 0x00
-	OUT		PORTB, R16 // Se configuran los pines para estar inicialmente apagados
+	OUT		PORTB, R16	// Se configuran los pines para estar inicialmente apagados
 
-	LDI		R17, 0xFF // Variable para guardar estado de botones
+	LDI		R17, 0xFF	// Variable para guardar estado de botones
+
+// Loop infinito
+MAIN:
+	IN		R16, PIND	// Se guarda el estado de PORTD en R16
+	CP		R17, R16	// Compara el estado anterior con el estado actual del pb1
+	BREQ	MAIN		// Si es el mismo estado repite los dos pasos anteriores
+	CALL	DELAY
+	IN		R16, PIND	// Lee nuevamente R16 para ver si no fue un error de lectura
+	CP		R17, R16
+	BREQ	MAIN		// Si fue un erro de lectura regresa a MAIN
+	MOV		R17, R16	// Guardamos el valor de la lectura anterior en R17
 
 
+// Sub-rutina (no de interrupcion)
+DELAY: // Se realiza un delay como medida antirrebote
+	LDI		R18, 0xFF
+	LDI		R19, 0x04	// Cargamos los valores necesarios a dos registros
+SUB_DELAY:
+	DEC		R18
+	CPI		R18, 0		
+	BRNE	SUB_DELAY	// Se resta 1 a R18 hasta que llegue a 0 y ignora el BRNE
+	DEC		R19
+	CPI		R19, 0		// Se resta 1 a R19 hasta que llegue a 0	
+	RET					// Al llegar R19 a 0 regresa a MAIN: CALL
 
