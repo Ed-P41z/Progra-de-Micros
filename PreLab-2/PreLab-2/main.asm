c7930998e19fc20091c010d5d954b251e59895b3
Ed-P41z <pai23072@uvg.edu.gt>
2025-02-08 17:13:03 -0600

    Implementación del sumador con timer
    Para realizar la implementación del sumador que sume cada 10ms, utilicé como base el ejemplo realizado en clase sobre el timer0.
    Para el prelaboratorio utilicé el Timer0 nuevamente, el prescaler está configurado a 64 y la frecuencia de oscilación es de 1MHz.
    Debido a que la manera de contar los 10ms es similar a la manera en la que se cuentan los 50ms del ejemplo en clase, las instrucciones son similares, pero en la línea 41, es necesario cambiar el valor de 50 a 10, esto debido a que los cálculos dan un aproximado de 9.92ms, que es un valor muy aproximado a los 10ms necesarios.
    Para implementar el sumador, utilicé un INC para aumentar el valor del contador de los Leds, luego verifiqué si había overflow y en caso que hubiera, le hice reset al contador y luego para ambos casos, si hay overflow o no, usé un OUT para mostrar el valor del contador en los Leds, es decir en PORTB.
    Además de esto, coloqué el módulo que permite inicial el timer 0 como sub-rutina.

--- PreLab-2/PreLab-2/main.asm
+++ PreLab-2/PreLab-2/main.asm
@@ -27,3 +27,35 @@ SETUP:
 
 	LDI		R17, 0x00	// Variable para guardar estado de contador de reloj
 	LDI		R18, 0x00	// Variable para guardar estado de Leds
+
+	
+// Loop infinito
+MAIN:
+	IN		R16, TIFR0	// Se lee la bandera del registro de interrupción
+	SBRS	R16, TOV0	// Se verifica que la bandera de overflow está encendida
+	RJMP	MAIN		// Si está apagada la bandera, regresa al inicio del loop
+	SBI		TIFR0, TOV0	// Si está encendida la bandera, salta a apagarla
+	LDI		R16, 100
+	OUT		TCNT0, R16	// Se vuelve a cargar un valor inicial a Timer0
+	INC		R17
+	CPI		R17, 10		// Se compara con 10 para verificar si pasaron 10ms
+	BRNE	MAIN		// Si no han pasado 10ms regresa a MAIN
+	CLR		R17			// Si ya pasaron 10ms limpia el registro del contador de reloj
+	INC		R18
+	CPI		R18, 0x10	
+	BREQ	OVERFLOW	// Se suma 1 a R18 y verifica si hay overflow
+	OUT		PORTD, R18
+	RJMP	MAIN		// Si no hay overflow imprime el valor y regresa a MAIN
+
+// Sub-rutina (no de interrupcion)
+INIT_TMR0:
+	LDI		R16, (1<<CS01) | (1<<CS00)
+	OUT		TCCR0B, R16	// Setear prescaler del TIMER 0 a 64
+	LDI		R16, 100
+	OUT		TCNT0, R16	// Cargar valor inicial en TCNT0
+	RET
+
+OVERFLOW:
+	LDI		R18, 0x00	
+	OUT		PORTD, R18	// Si hay overflow resetea el contador e imprime el valor
+	RJMP	MAIN
