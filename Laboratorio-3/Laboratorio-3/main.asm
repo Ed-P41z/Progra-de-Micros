073c9e18ba4d1a1496437c7cb809ae21ad17fa8b
Ed-P41z <pai23072@uvg.edu.gt>
2025-02-19 13:50:49 -0600

    Laboratorio
    Este es el código hecho dentro del tiempo del laboratorio. Se realizó el contador que aumenta cada segundo y funciona en paralelo con el contador que usa botones. Todo utiliza interrupciones.

---
+++ Laboratorio-3/Laboratorio-3/main.asm
@@ -0,0 +1,170 @@
+/*
+*	Laboratorio-3.asm*
+*	Creado: 2/17/2025 5:16:06 PM
+*	Autor: Edvin Paiz
+*	Descripción: El prelab 3 consiste en un sumador usando interrupciones
+*/
+
+.include "M328PDEF.inc"
+
+.cseg
+.org	0x0000
+	JMP		SETUP
+
+.org	0x0006
+	JMP		PBREAD		//Sub-rutina de interrupción cuando se presiones los botones
+
+.org	0x0020
+	JMP		TMR0_OV		//Sub-rutina de interrupción cuando hay overflow en el timer0
+
+
+SETUP:
+	// Se apagan las interrupciones globales
+	CLI
+
+	// Lista de valores para mostrar números en el display
+	Disp_Hex:	.DB 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F
+	//				 0		1	 2	   3	 4	   5	 6	   7	 8	   9
+
+	// Se configura la pila
+	LDI		R16, LOW(RAMEND)
+	OUT		SPL, R16
+	LDI		R16, HIGH(RAMEND)
+	OUT		SPH, R16
+	
+	// Se realiza la configuración del prescaler
+	LDI		R16, (1 << CLKPCE)
+	STS		CLKPR, R16	// Se habilita el cambio del prescaler
+	LDI		R16, 0x04
+	STS		CLKPR, R16	// Se configura el prescaler a 1MHz
+	CALL	INIT_TMR0	// Se inicia el timer 0 (es el timer que usaré)
+
+	// Desabilitar el serial
+	LDI R16, 0x00
+	STS UCSR0B, R16
+
+	// Se habilitan las interrupciones de Timer0
+	LDI		R16, (1 << TOIE0)
+	STS		TIMSK0, R16
+
+	// Se habilitan las interrupciones de Pin Change 0
+	LDI		R16, (1 << PCIE0)
+	STS		PCICR, R16
+	LDI		R16, (1 << PCINT0) | (1 << PCINT1)
+	STS		PCMSK0, R16
+
+	// Se configuran pines de entrada y salida (DDRx, PORTx, PINx)
+	// Se configura PORTD como salida inicialmente apagado
+	LDI		R16, 0xFF
+	OUT		DDRD, R16	// Se configura el puerto D como salida
+	OUT		DDRC, R16	// Se configura el puerto C como salida
+	LDI		R16, 0x00
+	OUT		PORTD, R16	
+	OUT		PORTC, R16	// Se configuran los pines para estar inicialmente apagados
+
+
+	// Se configura PORTB como entrada con pull-up habilitado
+	LDI		R16, 0x00
+	OUT		DDRB, R16	// Se configura el puerto B como entrada
+	LDI		R16, 0xFF
+	OUT		PORTB, R16	// Se configuran los pines con pull-up activado
+
+	LDI		R17, 0x00	// Variable para guardar estado de contador de reloj
+	LDI		R18, 0x00	// Variable para guardar estado de Leds
+	LDI		R19, 0xFF	// Variable para guardar estado de botones
+	LDI		R20, 0x00	// Variable para guardar contador 1
+	LDI		R21, 0x00	// Variable para guardar contador 2
+
+	CALL	INICIAR_DISP// Se inicia el display donde se mostrará el contador
+	
+	SEI					// Habilitamos las interrupciones globales nuevamente
+
+MAIN:
+ 	RJMP MAIN	// Para mantener entretenido el main loop
+
+	// Sub-rutina (no de interrupcion)
+INIT_TMR0:
+	LDI		R16, (1 << CS01) | (1 << CS00)
+	OUT		TCCR0B, R16	// Setear prescaler del TIMER0 a 64
+	LDI		R16, 100
+	OUT		TCNT0, R16	// Cargar valor inicial en TCNT0
+	RET
+
+INICIAR_DISP:	// Se modifica la dirección a la que apunta Z a la primera de la lista
+	LDI		ZL, LOW(Disp_Hex << 1)	
+	LDI		ZH, HIGH(Disp_Hex << 1)	// Se apunta a la primera dirección de Z
+	LPM		R16, Z		// Se carga el valor guardado en la primera dirección
+	OUT		PORTD, R16	// Se saca a PORTD el primer valor al que apunta Z
+	RET
+
+// Sub-rutina de interrupcion
+TMR0_OV:
+	SBI		TIFR0, TOV0	// Si está encendida la bandera de overflow, salta a apagarla
+	LDI		R16, 100
+	OUT		TCNT0, R16	// Se vuelve a cargar un valor inicial a Timer0 
+	INC		R17			// Incrementa cada 10ms
+	CPI		R17, 100	// Se compara con 100 para verificar si pasó 1 seg
+	BRNE	NOT_1S		// Si no ha pasado 1 seg regresa a MAIN
+	BREQ	SUMA
+	RETI
+
+NOT_1S:
+	RETI
+
+SUMA: // Se realiza la suma en R20 como sub-rutina
+	ADIW	Z, 1		// Se le suma 1 al valor al que apunta Z
+	INC		R20
+	CPI		R20, 0x0A	// Le sumamos 1 a R20 y comparamos si hay overflow
+	BREQ	OVERFLOW	// Si hay overflow, reinicia el sumador
+	LPM		R16, Z
+	OUT		PORTD, R16	// Sacamos el valor guardado en Z a PORTD
+	LDI		R17, 0
+	RETI
+OVERFLOW:
+	LDI		R20, 0x00	// Si hay overflow, hacemos reset al registro R20
+	CALL	INICIAR_DISP	// Se llama a la subrutina que reinicia el puntero de Z
+	LDI		R17, 0
+	RETI
+
+PBREAD:
+	// Esta es la medida antirebote utilizando el Timer0
+	IN		R16, TIFR0	// Se lee la bandera del registro de interrupción
+	SBRS	R16, TOV0	// Se verifica que la bandera de overflow está encendida
+	RJMP	PBREAD		// Si está apagada la bandera, regresa al inicio del loop (MAIN)
+	SBI		TIFR0, TOV0	// Si está encendida la bandera, salta a apagarla
+	LDI		R16, 158
+	OUT		TCNT0, R16	// Se vuelve a cargar un valor inicial a Timer0 
+	IN		R16, PINB	// Se guarda el estado de PORTB en R16
+	CP		R16, R19
+	BREQ	NOT_EQ		// Compara el estado anterior y el estado actual y verifica si son iguales
+	MOV		R19, R16	// Si son iguales guarda el estado de los botones
+	SBIS	PINB, PB0
+	RJMP	SUMA_C2		// En caso que se presione pb0: Suma, no: Salta
+	SBIS	PINB, PB1
+	RJMP	RESTA		// En caso que se presione pb1: Resta, no: Salta
+	RETI
+
+NOT_EQ:
+	RETI				// Si el estado es igual entonces sale de la sub-rutina
+
+SUMA_C2:
+	INC		R21
+	CPI		R21, 0x10	// Le sumamos 1 a R20 y comparamos si hay overflow
+	BREQ	OVERFLOW_C2	// Si hay overflow, reinicia el sumador
+	OUT		PORTC, R21	// Sacamos el valor guardado en R21 a PORTC
+	RETI
+OVERFLOW_C2:
+	LDI		R21, 0x00	// Si hay overflow, hacemos reset al registro R20
+	OUT		PORTC, R21	// Sacamos el valor guardado en R21 a PORTC
+	RETI
+
+RESTA:
+	DEC		R21
+	CPI		R21, 0xFF	// Le restamos 1 a R20 y comparamos si hay underflow
+	BREQ	UNDERFLOW	// Si hay underflow, setea el sumador
+	OUT		PORTC, R21	// Sacamos el valor guardado en r21 a PORTC
+	RETI
+UNDERFLOW:
+	LDI		R21, 0x0F	// Si hay underflow, dejamos en reset al registro R21
+	OUT		PORTC, R21	// Sacamos el valor guardado en R21 a PORTC
+	RETI
