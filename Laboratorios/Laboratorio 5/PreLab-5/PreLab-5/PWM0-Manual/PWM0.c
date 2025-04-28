03b5fd7adde041c02e9d6710e006097e079dea5f
Ed-P41z <pai23072@uvg.edu.gt>
2025-04-27 23:21:38 -0600

    Lab y postlab
    Para el lab se creó una función que ocupa la función del prelab para escribir un caracter, con la finalidad de repetirla hasta escribir una cadena de texto completa.
    Para el postlab, diseñó un menú usando escribir_cadena. Por medio de los valores ascii de 1 y 2 diseñé el modelo de interacción con el menú. Dependiendo de lo que se desee realizar con el menú,  se lee el potenciómetro para mostrar en los leds y en la terminal el valor binario y decimal respectivamente; o también se puede introducir un valor ascii para mostrarlo en los leds.

--- Laboratorios/Laboratorio 5/PreLab-5/PreLab-5/PWM0-Manual/PWM0.c
+++ Laboratorios/Laboratorio 5/PreLab-5/PreLab-5/PWM0-Manual/PWM0.c
@@ -14,8 +14,9 @@ void initPWM0A() // PWM0 manual usando timer0
 {
 	// El prescaler es el mismo del timer1 así que solamente le cargaré el mismo valor para que no haya problemas de prescaler
 	TCCR0A	|= 0;
-	TCCR0B	|= (1 << CS01); // Prescaler = 8
-	TCNT0	= 251;
+	TCCR0B	|= (1 << CS00); // Prescaler = 8
+	TIMSK0	= (1 << TOIE0) | (1 << OCIE0A);
+	//TCNT0	= 251;
 }
 
 void pwm0_cp(uint8_t top, uint8_t compare)
@@ -33,5 +34,5 @@ void pwm0_cp(uint8_t top, uint8_t compare)
 
 uint8_t adc0_map(uint8_t lec_adc0)
 {
-	return (lec_adc0 * 25UL) / 255;
+	return (lec_adc0 * 200UL) / 255;
 }
--- Laboratorios/Laboratorio 6/PreLab-6/PreLab-6/main.c
+++ Laboratorios/Laboratorio 6/PreLab-6/PreLab-6/main.c
@@ -11,12 +11,26 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 char recibido;
+char recibido_b;
+char recibido_d;
+uint8_t adc_b;
+uint8_t adc_d;
+uint8_t mostrar_menu;
+uint8_t lec_adc;
+uint8_t u_adc;
+uint8_t d_adc;
+uint8_t c_adc;
+uint8_t envio_ascii;
+uint8_t recibido_ascii;
+uint8_t verif;
 
 //*********************************************
 // Function prototypes
 void setup();
+void initADC();
 void initUART();
 void writeChar(char caracter);
+void escribir_cadena(char* cadena);
 
 //*********************************************
 // Main Function
@@ -25,10 +39,57 @@ int main(void)
 {
 	setup();	// Se manda a llamar la función de Setup
 	
-	writeChar('s');
-	
 	while(1)	// Entra al bucle infinito en donde se ejecuta el programa
 	{
+		if (mostrar_menu)
+		{
+			escribir_cadena("\n -------------------------------------------------------------------- ");
+			escribir_cadena("\n Presione 1 o 2 para seleccionar una opción: ");
+			escribir_cadena("\n 1- Leer Potenciómetro.");
+			escribir_cadena("\n 2- Enviar Ascii.");
+			mostrar_menu = 0;  // Ya se mostró el menú, no volver a mostrar hasta que sea necesario
+		}
+		else if (recibido == 49)  // Si se recibe '1', se ejecuta la opción de lectura del potenciómetro
+		{
+			escribir_cadena("\n Lectura Potenciómetro: ");
+			
+			adc_b = lec_adc >> 2;    // Los 6 bits más significativos se asignan a PORTB
+			adc_d = (lec_adc & 0x03) << 6;  // Los 2 bits menos significativos se asignan a PORTD (desplazados a la izquierda)
+			
+			PORTB = adc_b;  // Se actualiza PORTB con los 6 bits más significativos
+			PORTD = adc_d;  // Se actualiza PORTD con los 2 bits menos significativos
+			
+			u_adc = lec_adc % 10;         // División a Unidades
+			d_adc = (lec_adc % 100) / 10; // División a Decenas
+			c_adc = lec_adc / 100;        // División a Centenas
+			
+			writeChar(c_adc + '0'); // Se escribe el número en ascii
+			writeChar(d_adc + '0'); // Se escribe el número en ascii
+			writeChar(u_adc + '0'); // Se escribe el número en ascii
+			
+			recibido = 0;       // Se resetea para comenzar nuevamente lógica de menú
+			mostrar_menu = 1;   // Se setea para mostrar el menú nuevamente en el primer if
+		}
+		else if (recibido == 50)  // Si se recibe '2', se ejecuta la opción de enviar un ASCII
+		{
+			escribir_cadena("\n Envíe un Ascii: ");
+			envio_ascii = 1;        // Activar modo de recepción especial
+			recibido = 0;           // Limpiar para no confundir
+
+			while (verif != 1);    // Se espera para recibir el ascii
+			
+			writeChar('c');    // Envía un 'c' como confirmación de recepción de ASCII
+			
+			recibido_b = recibido_ascii >> 2;   // Los 6 bits más significativos se asignan a PORTB
+			recibido_d = (recibido_ascii & 0x03) << 6; // Los 2 bits menos significativos se asignan a PORTD (desplazados a la izquierda)
+			
+			PORTB = recibido_b;  // Se actualiza PORTB con los 6 bits más significativos del ASCII recibido
+			PORTD = recibido_d;  // Se actualiza PORTD con los 2 bits menos significativos del ASCII recibido
+
+			mostrar_menu = 1;       // Se vuelve al menú principal
+			recibido = 0;
+			verif = 0;
+		}
 	}	
 }
 
@@ -45,16 +106,21 @@ void setup()
 	// Configuración de Pines
 	DDRB	= 0xFF;			// Se configura PORTB como salida
 	PORTB	= 0x00;			// PORTB inicialmente apagado
-	//DDRC	= 0x00;			// Se configura PORTC como entrada
-	//PORTC	= 0x00;			// PORTC sin pull-up
+	DDRD	= 0xFF;			// Se configura PORTD como salida
+	PORTD	= 0x00;			// PORTD inicialmente apagado
+	DDRC	= 0x00;			// Se configura PORTC como entrada
+	PORTC	= 0x00;			// PORTC sin pull-up
 	
 	// Inicialización de Variables
+	mostrar_menu = 1;
+	envio_ascii = 0;
+	recibido_ascii = 0;
 	
 	// Inicio de Timers
 	
 	// Inicio de ADC
-	//initADC();
-	//ADCSRA	|= (1 << ADSC);	// Se hace la primera lectura del ADC
+	initADC();
+	ADCSRA	|= (1 << ADSC);	// Se hace la primera lectura del ADC
 	
 	// Inicio de UART
 	initUART();
@@ -64,7 +130,7 @@ void setup()
 	sei(); // Se encienden las interrupciones globales
 }
 
-/*void	 initADC()
+void initADC()
 {
 	ADMUX	= 0;
 	ADMUX	|= (1 << REFS0);
@@ -75,7 +141,7 @@ void setup()
 	ADCSRA	|= (1 << ADPS2) | (1 << ADPS1);
 	ADCSRA	|= (1 << ADIE);
 	ADCSRA	|= (1 << ADEN);					// Se configura la interrupción y el prescaler
-}*/
+}
 
 void initUART()
 {
@@ -98,9 +164,16 @@ void initUART()
 
 void writeChar(char caracter)
 {
-	// uint16_t temporal = UCSR0A & (1 << UDRE0);
-	while ((UCSR0A & (1 << UDRE0)) == 0);
-	UDR0 = caracter;
+	while ((UCSR0A & (1 << UDRE0)) == 0);  // Esperar a que el registro de datos esté vacío
+	UDR0 = caracter;  // Enviar el caracter al registro de UART
+}
+
+void escribir_cadena (char* cadena)
+{
+	for (uint8_t puntero = 0; *(cadena+puntero) != '\0'; puntero++)  // Itera sobre cada carácter de la cadena
+	{
+		writeChar(*(cadena+puntero));  // Enviar cada carácter a través de UART
+	}
 }
 
 //*********************************************
@@ -108,13 +181,17 @@ void writeChar(char caracter)
 ISR(USART_RX_vect)
 {
 	recibido = UDR0;  // Leer el carácter recibido desde el registro de UART
-	PORTB = recibido;      // Mostrarlo en el puerto B
+	
+	if (envio_ascii == 1)
+	{
+		recibido_ascii = recibido;  // Se guarda el valor recibido 
+		envio_ascii = 0;	// Se apaga la bandera para no reingresar al if en el menú principal
+		verif = 1;			// Se activa la verificación para salir del bucle principal en el main
+	}
 }
 
-/*ISR(ADC_vect)
+ISR(ADC_vect)
 {
-	ADCSRA	|= (1 << ADSC);				// Se realiza la lectura de ADC
-}*/
-
-
-
+	lec_adc = ADCH;			// Se guarda ADCH
+	ADCSRA	|= (1 << ADSC);	// Se realiza la lectura de ADC
+}
